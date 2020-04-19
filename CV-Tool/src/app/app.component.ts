8bacfe3c184e8197d4b6eaf002d520c482d95b8f
Qlassy <alex.tellier01@gmail.com>
2020-04-19 09:06:00 -0400

    left screen is now styled

--- CV-Tool/src/app/app.component.ts
+++ CV-Tool/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { ToolbarComponent } from './toolbar/toolbar-component/toolbar-component.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+@Injectable({
+  providedIn: 'root'
+})
+
 export class AppComponent {
   title = 'CV-Tool';
+
+  router: {
+    position: string
+    left: string
+  };
+
+  constructor(public toolbarComponent: ToolbarComponent) {
+    this.router = {
+      'position': 'absolute',
+      'left': `${toolbarComponent.width}` + 'px'
+    }
+  }
 }
--- CV-Tool/src/app/toolbar/toolbar-component/toolbar-component.component.ts
+++ CV-Tool/src/app/toolbar/toolbar-component/toolbar-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 import { ToolbarElements } from './../../models/toolbars-elements-interface/toolbar-elements';
 
 @Component({
@@ -6,15 +6,21 @@ import { ToolbarElements } from './../../models/toolbars-elements-interface/tool
   templateUrl: './toolbar-component.component.html',
   styleUrls: ['./toolbar-component.component.scss']
 })
+
+@Injectable({
+  providedIn: 'root'
+})
+
 export class ToolbarComponent implements OnInit {
 
   selectedElement: ToolbarElements;
-
+  width: number;
   toolbarWidth: { width: string; }
 
   constructor() {
+    this.width = 66;
     this.toolbarWidth = {
-      'width': '66px'
+      'width': `${this.width}` + 'px'
     }
   }
 
